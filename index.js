f9324c8065a6b501ac0d629f58cc3aeff78df400
tmchein <davidr2998@gmail.com>
2020-10-22 16:09:42 -0500

    Se cambia la manera en la que se utiliza el token, se utiliza dotenv en este caso

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require("dotenv").config({ path: "variables.env" });
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Bot is ready as ${client.user.tag}`);
-  client.user.setStatus("idle");
+  client.user.setStatus("online");
   console.log(client.user.presence.status);
 });
 
-client.login("NzY4OTI1MjA1NzU3ODIwOTU4.X5HjZg.GOUShOPaRfY9kTsVMbcD35dXT3I");
+client.login(process.env.CONTRA_DISCORD);
 
 client.on("message", (message) => {
   console.log(message.content);
