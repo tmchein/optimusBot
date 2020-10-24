c43e0736155816e4eb77ceae3073db32db1007e2
Jhosua Pachon <jhosuapachon@gmail.com>
2020-10-24 13:51:50 -0500

    ADD: command skip

---
+++ src/commands/skip.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "skip",
+  execute(client, message, args, songlist) {
+    let data = songlist.get(message.guild.id);
+
+    /* Comprueba si el usuario esta en un canal */
+    if (!message.member.voice.channel) {
+      return message.channel.send("Debes estar en un canal de voz para saltar la música");
+    }
+    /* Valida si hay una canción reproduciendose */
+    if (!data) {
+      return message.channel.send("Actualmente no se esta reproduciendo música");
+    }
+    /* salta a la siguiente canción */
+    if (data.queue) {
+      data.connection.dispatcher.end();
+      message.channel.send("He saltado la música por ti");
+    }
+    return undefined;
+  }
+};
