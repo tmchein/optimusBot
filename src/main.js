cfb55e3e4ac0c398f83b3715576dadda4f791367
Jhosua Pachon <jhosuapachon@gmail.com>
2020-10-29 16:36:04 -0500

    REFACTOR: updated main file

--- src/main.js
+++ src/main.js
@@ -3,42 +3,12 @@ const fs = require("fs");
 const path = require("path");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-/* Libreria de Twitter */
-const Twitter = require("twitter");
-
-/* Inicializacion de twitter */
-let lector = new Twitter({
-  consumer_key: process.env.KEY_TWITTER,
-  consumer_secret: process.env.SECRET_KEY_TWITTER,
-  access_token_key: process.env.TOKEN_TWITTER,
-  access_token_secret: process.env.TOKEN_SECRET_TWITTER
-});
-
-/* Funcion que trae los ultimos 20 tweets y publica un tweet random */
-function getTweet(message) {
-  let random = Math.floor(Math.random() * (20 - 0)) + 0;
-  let params = { screen_name: "Johanv65" };
-  lector.get("statuses/user_timeline", params, function (error, tweets, response) {
-    if (!error) {
-      const embed = new Discord.MessageEmbed()
-        .setTitle("Twitazo")
-        .setDescription(tweets[random].text)
-        .setColor(tweets[random].user.profile_background_color)
-        .setAuthor(tweets[random].user.name, tweets[random].user.profile_image_url)
-        .setFooter(tweets[random].created_at);
-      message.channel.send(embed);
-    }
-  });
-}
+const PREFIX = process.env.PREFIX;
 
 client.commands = new Discord.Collection();
 
 /* Requiere de forma dinamica los archivos terminados en .js en la carpeta commands */
 const commandFiles = fs.readdirSync(path.join(__dirname, "./commands")).filter((file) => file.endsWith(".js"));
-
-/* Prefijo establecido */
-const PREFIX = "!";
 
 let songList = new Map();
 
@@ -59,7 +29,7 @@ client.on("message", async (message) => {
   const cmd = args.shift().toLowerCase();
 
   switch (cmd) {
-    case "play":
+    case ("play", "p"):
       client.commands.get("play").execute(client, message, args, songList);
       break;
     case "leave":
@@ -78,7 +48,17 @@ client.on("message", async (message) => {
       client.commands.get("help").execute(client, message, args, songList);
       break;
     case "twitazo":
-      getTweet(message);
+      client.commands.get("twitazo").execute(client, message, args, songList);
+      break;
+    case "rol":
+      client.commands.get("rol").execute(client, message, args, songList);
+      break;
+    case "pc":
+      client.commands.get("pc").execute(client, message, args, songList);
+      break;
+    case "dr":
+      client.commands.get("dr").execute(client, message, args, songList);
+      break;
     default:
       break;
   }
